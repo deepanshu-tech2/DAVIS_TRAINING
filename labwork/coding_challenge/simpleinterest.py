15ab8fe5c6321d700b450aab1bfdc5e3daa1ddd5
deepanshu-tech1 <deepanshujatavd895@gmail.com>
2026-03-18 09:43:13 +0530

    simple interest

---
+++ labwork/coding_challenge/simpleinterest.py
@@ -0,0 +1,5 @@
+priciple = int(input("enter the principle amount"))
+rate  = int(input("enter the rate in %"))
+time = int(input("enter the time in year=>"))
+simple_interest = (priciple*time*rate)/100
+print("total interest is => ",simple_interest)
