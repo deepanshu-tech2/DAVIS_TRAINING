df943e698e7ef5e68e43dd0fde1b55e88c14952e
deepanshu-tech1 <deepanshujatavd895@gmail.com>
2026-03-18 11:06:59 +0530

    simple interest using function

---
+++ labwork/coding_challenge/si.py
@@ -0,0 +1,8 @@
+def si(p,r,t):
+    print("simple interest are =>" ,(p*r*t)/100)
+
+
+p = int(input("enter the principle amount => "))
+r = int(input("enter the rate percentage in year=>   "))
+t = int(input("enter the time in year=>  "))
+si(p,r,t)
