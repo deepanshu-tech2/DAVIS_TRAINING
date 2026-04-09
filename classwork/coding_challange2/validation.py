62fac6a4a7d7ecf37579754d75489b06258769cd
deepanshu-tech2 <your-github-email@example.com>
2026-04-09 09:54:10 +0530

    validtaion check

---
+++ classwork/coding_challange2/validation.py
@@ -0,0 +1,13 @@
+# Predefined username and password
+correct_username = "admin"
+correct_password = "1234"
+
+# Taking input from user
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+# Checking credentials
+if username == correct_username and password == correct_password:
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
