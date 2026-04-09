c7c2463de161c53bc782353205865db5eff17dd8
deepanshu-tech2 <your-github-email@example.com>
2026-04-09 10:16:00 +0530

    check number is zero,negative,positive

---
+++ classwork/coding_challange2/numbercheck.py
@@ -0,0 +1,7 @@
+a = int(input("enter a integar number => "))
+if(a==0):
+    print("this number is zero=>",a)
+elif(a>0):
+    print("this number is positive=>",a)
+else:
+    print("this number is negative =>",a)     
