2c0b734be3025b14b55039ce9f14b6d4b50238c0
deepanshu-tech2 <your-github-email@example.com>
2026-04-09 09:39:26 +0530

    bonus calculation

---
+++ classwork/coding_challange2/BonusCalculate.py
@@ -0,0 +1,9 @@
+# write a program to calculate the bonus amount using given rules 
+# Program to calculate bonus
+
+salary = float(input("Enter salary: "))
+
+# Bonus is 7% of salary
+bonus = salary * 0.07
+
+print("Bonus =", bonus)
