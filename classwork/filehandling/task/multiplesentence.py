54ecc9e227d61455164c937e3548615b88c518f4
deepanshu-tech2 <your-github-email@example.com>
2026-04-10 11:00:04 +0530

    a

--- classwork/filehandling/task/multiplesentence.py
+++ classwork/filehandling/task/multiplesentence.py
@@ -1,14 +1,31 @@
 #  every sentence must be in new line 
-sentence = input("enter the 20 sentence and must be seprated by comma all => ")
-lines = sentence.split(" ")
-lines = [line.strip() +"\n"for line in lines]
+# sentence = input("enter the 20 sentence and must be seprated by comma all => ")
+# lines = sentence.split(" ")
+# lines = [line.strip() +"\n"for line in lines]
+# with open("nextline.txt","w") as file:
+#     file.writelines(lines)
+#     print("file content add succesfully")
+#     file.seek(0)
+
+# with open("nextline.txt","r") as file:
+#     print("file content => \n")
+#     print(file.readlines())    
+
+# file.close()    
+
+word = []
+for i in range(20):
+    data= input()
+    word.append(data +"\n")
+
 with open("nextline.txt","w") as file:
-    file.writelines(lines)
+    file.writelines(word)
     print("file content add succesfully")
     file.seek(0)
+file.close()
 
-with open("nextline.txt","r") as file:
-    print("file content => \n")
-    print(file.read())    
+with open ("nextline.txt","r") as file:
+    print("file content is =>")
+    print(file.read())
 
 file.close()    
---
+++ classwork/filehandling/task/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+
+# word = []
+# for i in range(20):
+#     data= input()
+#     word.append(data +"\n")
+
+# with open("nextline.txt","w") as file:
+#     file.writelines(word)
+#     print("file content add succesfully")
+#     file.seek(0)
+# file.close()
