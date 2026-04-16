099154e019f54f70a2a34fda04b1667a40d05aad
deepanshu-tech2 <your-github-email@example.com>
2026-04-16 09:58:22 +0530

    student task if number >75 stored in another file other wise same file

---
+++ classwork/studenttask/student.py
@@ -0,0 +1,29 @@
+while True:
+    print("1: Enter student data")
+    print("0: Exit")
+
+    n = int(input("Enter choice (1/0): "))
+
+    if n == 0:
+        break
+
+    if n == 1:
+        name = input("Enter student name: ")
+        marks = int(input("Enter student marks: "))
+        city = input("Enter student city: ")
+
+        # dictionary
+        student = {
+            "name": name,
+            "marks": marks,
+            "city": city
+        }
+
+        if marks > 75:
+            with open("student75.txt", "a") as f:
+                f.write(str(student) + "\n")
+        else:
+            with open("student.txt", "a") as f:
+                f.write(str(student) + "\n")
+
+        print("Data saved successfully!\n")
